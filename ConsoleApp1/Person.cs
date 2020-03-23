998787db2e39902c6b06ab00e225540864f79e49
Simona Colapicchioni <s.colapicchioni@gmail.com>
2020-03-23 11:11:19 +0100

    day 1 - 11uur

---
+++ ConsoleApp1/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1 {
+    enum ItalianDays {
+        Lunedi, Martedi, Mercoledi
+    }
+
+    struct Person {
+        public int Id;
+        public string Name;
+        public string Surname;
+        public ItalianDays FavoriteDay;
+        public void DoSomething() {
+            //string result = "hi, my name is " + Name + " and my surname is " + Surname;
+
+            //result = string.Format("hi, my name is {0} and my surname is {1}",Name,Surname) ;
+
+            //result = $"hi, my name is {Name} and my surname is {Surname}";
+
+            //Console.WriteLine("hi, my name is {0} and my surname is {1}", Name, Surname);
+
+            Console.WriteLine($"hi, my name is {Name} and my surname is {Surname}");
+            
+
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,9 +1,107 @@
 ﻿using System;
+using System.Drawing;
+using System.Net.Http;
+using System.Text;
 
 namespace ConsoleApp1 {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            //type name;
+            //name = expression;
+
+            //type name = expression;
+
+            //value types ==> struct, enum
+            //reference types ==> class, arrays, delegates
+
+            int a =5; //inline memory
+
+            long l = long.MaxValue;
+
+            a = (int)l;
+
+            //bool b = (bool)a;
+
+            //bool b = 1;
+
+
+            a = 3;
+
+            a = 5;
+            int b = 10;
+
+            b = a++;
+
+            b = ++a;
+
+            b = a > 5 ? 10 : 20;
+
+            char c1 = 'a';
+            //c1 = "c";
+
+
+            //string s1 = 'asdg';
+            
+            string s1 = "asdg"; //
+
+            s1 = s1 + "aa"; //asdgaa
+
+            StringBuilder builder = new StringBuilder();
+
+            builder.Append("aa");
+            builder.Append("aa");
+            builder.Append("aa");
+
+            string s2 = builder.ToString();
+
+            s2.Trim();
+
+            s1 = a.ToString();
+
+            s1 = "5";
+
+            if (int.TryParse(s1, out a)) { 
+                
+            }
+            
+            //enum
+
+            Method02();
+
+            Point p = new Point(3, 4);
+
+            Console.BackgroundColor = ConsoleColor.Yellow;
+
+            ItalianDays day = ItalianDays.Lunedi;
+
+            ItalianDays day2 = day;
+
+            day = ItalianDays.Martedi;
+
+
+            int d = (int)day;
+
+            Console.WriteLine(day);
+
+            Person simo;
+            simo.Id = 5;
+            simo.Name = "Simona";
+            simo.Surname = "Colapicchioni";
+            simo.FavoriteDay = ItalianDays.Mercoledi;
+
+            simo.DoSomething();
+
+            Console.WriteLine("What's the id?");
+
+            simo.Id = int.Parse(Console.ReadLine());
+
+
+        }
+
+        static void Method02() {
+            int a = 5; 
         }
     }
+
+    
 }
